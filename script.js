049339aac043e9ca37ad1d6372ca7bd13a3ccaef
tarun <tarunjangra1903@gmail.com>
2025-05-29 00:47:40 +0530

    Todo app

--- script.js
+++ script.js
@@ -1,38 +1,63 @@
 const addButton = document.getElementsByClassName("addButton")[0];
 const inputFromUser = document.getElementsByClassName("userinput")[0];
 const taskList = document.getElementsByClassName("tasklist")[0];
+const align = document.getElementsByClassName("align")[0];
+const deleteAll = document.getElementsByClassName("deleteAll")[0];
+deleteAll.addEventListener("click", function () {
+  const task = document.querySelectorAll(".tasklist li")
+  if (task.length<=2) {
+  task.style.color = "green"
+  }
+  if (task.length>=2) {
+    // task.remove()
+    task.forEach(task => task.remove());
+    // task.style.display = "none"
 
+  // if (confirm("Delete ALL tasks permanently?")) {
+  //   // toggleDeleteAllButton();
+  // }
+}})
+inputFromUser.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    addButton.click();
+  }
+});
+const list = function deleteAll() {
+  
+}
 addButton.addEventListener("click", () => {
   const taskText = inputFromUser.value.trim();
-
+  
   if (taskText !== "") {
-    // Create <li>
     const li = document.createElement("li");
-
-    // Create <div class="learnjs">
     const div = document.createElement("div");
     div.classList.add("learnjs");
-
-    // Create checkbox input
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-
-    // Create <p> for task text
     const p = document.createElement("p");
     p.textContent = taskText;
-
-    // Append input and p to div
+    const addCross = document.createElement("button");
+    addCross.setAttribute("class", "cross");
+    addCross.innerText = "❌";
     div.appendChild(checkbox);
     div.appendChild(p);
-
-    // Append div to li
+    div.appendChild(addCross);
     li.appendChild(div);
-
-
+    console.log(addCross);
     taskList.appendChild(li);
-    
+    addCross.onclick = function () {
+      li.remove();
+    };
     inputFromUser.value = "";
   } else {
     alert("Enter a task");
   }
 });
+// deleteAll.addEventListener("click", () => {
+//  const list = document.createElement("li");
+//   if(list>=2){
+//     deleteAll.style.display = "none"
+//   }else{
+//     taskList.remove();
+//   }
+// })
